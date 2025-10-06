e96a0dcc17d1350bb85dafbf09048b84077f15f9
mayli4 <sydsleeper@gmail.com>
2025-10-06 17:19:04 -0400

    feat: native libs

---
+++ src/FnaTemplate/Entry.cs
@@ -0,0 +1,15 @@
+﻿using FnaTemplate._Driver;
+
+namespace FnaTemplate;
+
+file class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        FnaBootstrap.Bootstrap();
+        
+        var main = new Main();
+        main.Run();
+    }
+}
---
+++ src/FnaTemplate/Main.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FnaTemplate;
+
+public sealed class Main : Game
+{
+    public static GraphicsDeviceManager Graphics;
+    public static SpriteBatch SpriteBatch;
+    
+    public Main()
+    {
+        Graphics = new GraphicsDeviceManager(this);
+        Graphics.PreferredBackBufferWidth = 1280;
+        Graphics.PreferredBackBufferHeight = 720;
+        Graphics.PreferMultiSampling = true;
+
+        Window.AllowUserResizing = true;
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        SpriteBatch  = new SpriteBatch(GraphicsDevice);
+        
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        
+        base.Draw(gameTime);
+    }
+}
